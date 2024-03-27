b5cc692bcff909c36b4bccd1df45346d185eaf90
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2024-03-27 17:22:16 +0600

    create cover section with parallex blur

--- src/Pages/MenuPage/Menu.jsx
+++ src/Pages/MenuPage/Menu.jsx
@@ -1,9 +1,10 @@
-
+import menuImg from "../../assets/menu/banner3.jpg"
+import Cover from "../SharePage/Cover/Cover";
 
 const Menu = () => {
     return (
         <div>
-            
+            <Cover img={menuImg} title={"our menu"} para={"Would you like to try a dish?"}></Cover>
         </div>
     );
 };
--- src/Pages/SharePage/Cover/Cover.jsx
+++ src/Pages/SharePage/Cover/Cover.jsx
@@ -1,8 +1,23 @@
+import { Parallax} from 'react-parallax';
 
 
-const Cover = () => {
+// eslint-disable-next-line react/prop-types
+const Cover = ({img,title,para}) => {
     return (
         <div>
+             <Parallax
+        blur={{ min: -50, max: 50 }}
+        bgImage={img}
+        bgImageAlt="the menu"
+        strength={-200}
+    >
+        <div className='h-[300px] text-white flex flex-col items-center mt-36 bg-slate-500 bg-opacity-20'>
+            <h1 className='text-3xl uppercase'>{title}</h1>
+            <h4 className='capitalize '> {para}</h4>
+
+        </div>
+       
+    </Parallax>
             
         </div>
     );
