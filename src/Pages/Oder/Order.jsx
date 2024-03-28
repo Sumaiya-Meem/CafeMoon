efbd63ca060e7fa1235f78b8daf744510dd67468
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2024-03-28 22:22:28 +0600

    load menu item in order section

--- src/Pages/Oder/Order.jsx
+++ src/Pages/Oder/Order.jsx
@@ -3,9 +3,16 @@ import img from "../../assets/shop/banner2.jpg";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import { useState } from "react";
+import useMenu from "../Hooks/useMenu";
 
 const Order = () => {
+  const [menu]=useMenu();
   const [tabIndex, setTabIndex] = useState(0);
+
+  const dessert=menu.filter(item=>item.category==="dessert");
+  const soup=menu.filter(item=>item.category==="soup");
+  const salad=menu.filter(item=>item.category==="salad");
+  
   return (
     <div>
       <Cover img={img} title={"ORDER NOW"} para={""}></Cover>
