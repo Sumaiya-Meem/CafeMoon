86d9254c89ddbd2353ccd8a22380036632044dc2
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2024-03-28 22:11:38 +0600

    set tab name according menu category

--- src/Pages/Oder/Order.jsx
+++ src/Pages/Oder/Order.jsx
@@ -1,12 +1,28 @@
 import Cover from "../SharePage/Cover/Cover";
-import img from "../../assets/shop/banner2.jpg"
+import img from "../../assets/shop/banner2.jpg";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import { useState } from "react";
 
 const Order = () => {
-    return (
-        <div>
-            <Cover img={img} title={"ORDER NOW"} para={""}></Cover>
-        </div>
-    );
+  const [tabIndex, setTabIndex] = useState(0);
+  return (
+    <div>
+      <Cover img={img} title={"ORDER NOW"} para={""}></Cover>
+      <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+        <TabList>
+          <Tab>SALAD</Tab>
+          <Tab>PIZZA</Tab>
+          <Tab>DESSERT</Tab>
+          <Tab>SOUP</Tab>
+        </TabList>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+      </Tabs>
+    </div>
+  );
 };
 
 export default Order;
