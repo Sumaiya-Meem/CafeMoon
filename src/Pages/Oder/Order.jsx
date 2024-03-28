9dc1058f03f61009713c7eb95268ae041b9a5090
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2024-03-28 22:03:52 +0600

    add cover section in order page

--- src/Pages/Oder/Order.jsx
+++ src/Pages/Oder/Order.jsx
@@ -1,9 +1,10 @@
-
+import Cover from "../SharePage/Cover/Cover";
+import img from "../../assets/shop/banner2.jpg"
 
 const Order = () => {
     return (
         <div>
-            
+            <Cover img={img} title={"ORDER NOW"} para={""}></Cover>
         </div>
     );
 };
